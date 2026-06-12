abb6fcc16bb9e90c25b11864f5c92338ba8afe15
AyamBercakap <bercakapayam@gmail.com>
2026-06-12 09:08:02 +0800

    Added DB, aspx is just for testing, ignore

--- WADevelopment/StockManage.aspx
+++ WADevelopment/StockManage.aspx
@@ -1,15 +1,68 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StockManage.aspx.cs" Inherits="WADevelopment.StockManage" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StockManage.aspx.cs" Inherits="WADevelopment.StockManage" EnableViewState="true" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Product Management</title>
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
+<form id="form1" runat="server">
+
+    <h2>Product Management</h2>
+
+    <asp:HiddenField ID="hfProductID" runat="server" Value="0" />
+
+    <div>
+        <label>SKU:</label>
+        <asp:TextBox ID="txtSKU" runat="server" />
+        <asp:RequiredFieldValidator runat="server" ControlToValidate="txtSKU"
+            ErrorMessage="Required" ForeColor="Red" Display="Dynamic" />
+    </div>
+    <div>
+        <label>Product Name:</label>
+        <asp:TextBox ID="txtProductName" runat="server" />
+        <asp:RequiredFieldValidator runat="server" ControlToValidate="txtProductName"
+            ErrorMessage="Required" ForeColor="Red" Display="Dynamic" />
+    </div>
+    <div>
+        <label>Unit Price (RM):</label>
+        <asp:TextBox ID="txtUnitPrice" runat="server" />
+        <asp:RequiredFieldValidator runat="server" ControlToValidate="txtUnitPrice"
+            ErrorMessage="Required" ForeColor="Red" Display="Dynamic" />
+        <asp:CompareValidator runat="server" ControlToValidate="txtUnitPrice"
+            Operator="DataTypeCheck" Type="Double"
+            ErrorMessage="Must be a number" ForeColor="Red" Display="Dynamic" />
+    </div>
+    <div>
+        <asp:Button ID="btnSave" runat="server" Text="Add Product" OnClick="btnSave_Click" />
+        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" CausesValidation="false" />
+    </div>
+
+    <br />
+    <asp:Label ID="lblMsg" runat="server" ForeColor="Green" /><br /><br />
+
+    <asp:GridView ID="gvProducts" runat="server" AutoGenerateColumns="false"
+        OnRowCommand="gvProducts_RowCommand" DataKeyNames="ProductID">
+        <Columns>
+            <asp:BoundField DataField="SKU" HeaderText="SKU" />
+            <asp:BoundField DataField="ProductName" HeaderText="Product Name" />
+            <asp:BoundField DataField="UnitPrice" HeaderText="Unit Price (RM)" DataFormatString="{0:F2}" />
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Button runat="server" Text="Edit" CommandName="EditProduct"
+                        CommandArgument="<%# Container.DataItemIndex %>" />
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Button runat="server" Text="Delete" CommandName="DeleteProduct"
+                        CommandArgument="<%# Container.DataItemIndex %>" />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+
+</form>
 </body>
 </html>
--- WADevelopment/StockManage.aspx.cs
+++ WADevelopment/StockManage.aspx.cs
@@ -1,17 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Web.UI.WebControls;
 
 namespace WADevelopment
 {
     public partial class StockManage : System.Web.UI.Page
     {
+        string connStr => ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                LoadProducts();
+        }
 
+        void LoadProducts()
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            using (SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Products ORDER BY ProductName", con))
+            {
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvProducts.DataSource = dt;
+                gvProducts.DataBind();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            int productID = int.Parse(hfProductID.Value);
+
+            if (productID == 0)
+            {
+                using (SqlConnection con = new SqlConnection(connStr))
+                using (SqlCommand chk = new SqlCommand("SELECT ProductID FROM Products WHERE SKU=@sku", con))
+                {
+                    chk.Parameters.AddWithValue("@sku", txtSKU.Text.Trim());
+                    con.Open();
+                    object result = chk.ExecuteScalar();
+                    if (result != null)
+                        productID = int.Parse(result.ToString());
+                }
+            }
+
+            string sql = productID == 0
+                ? "INSERT INTO Products (SKU, ProductName, UnitPrice) VALUES (@sku, @name, @price)"
+                : "UPDATE Products SET SKU=@sku, ProductName=@name, UnitPrice=@price WHERE ProductID=@id";
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(sql, con))
+            {
+                cmd.Parameters.AddWithValue("@sku", txtSKU.Text.Trim());
+                cmd.Parameters.AddWithValue("@name", txtProductName.Text.Trim());
+                cmd.Parameters.AddWithValue("@price", decimal.Parse(txtUnitPrice.Text));
+                if (productID != 0) cmd.Parameters.AddWithValue("@id", productID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            lblMsg.ForeColor = System.Drawing.Color.Green;
+            lblMsg.Text = productID == 0 ? "Product added." : "Product updated.";
+            ClearForm();
+            LoadProducts();
+        }
+
+        protected void gvProducts_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int rowIndex = int.Parse(e.CommandArgument.ToString());
+            int productID = int.Parse(gvProducts.DataKeys[rowIndex].Value.ToString());
+
+            if (e.CommandName == "EditProduct")
+            {
+                using (SqlConnection con = new SqlConnection(connStr))
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Products WHERE ProductID=@id", con))
+                {
+                    cmd.Parameters.AddWithValue("@id", productID);
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        hfProductID.Value = productID.ToString();
+                        txtSKU.Text = dr["SKU"].ToString();
+                        txtProductName.Text = dr["ProductName"].ToString();
+                        txtUnitPrice.Text = dr["UnitPrice"].ToString();
+                        btnSave.Text = "Update Product";
+                    }
+                }
+                lblMsg.Text = "";
+            }
+            else if (e.CommandName == "DeleteProduct")
+            {
+                using (SqlConnection con = new SqlConnection(connStr))
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Products WHERE ProductID=@id", con))
+                {
+                    cmd.Parameters.AddWithValue("@id", productID);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                lblMsg.ForeColor = System.Drawing.Color.Green;
+                lblMsg.Text = "Product deleted.";
+                LoadProducts();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e) => ClearForm();
+
+        void ClearForm()
+        {
+            hfProductID.Value = "0";
+            txtSKU.Text = txtProductName.Text = txtUnitPrice.Text = "";
+            btnSave.Text = "Add Product";
         }
     }
 }
